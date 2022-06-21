2b6f7ec4714f78927c06a00c23ca7ca82519f46d
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-06-21 21:47:52 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,15 +1,8 @@
 let num = 12;
-
-let tek;
-
-
-while(num>0){
-    // debugger;
-    // let tekinteki = num;
-   if(num%2!=0){
-    tek = num;
-    console.log(num);
-    
-   }
-   num--;
+let tek= 0;
+while(tek<num){
+    tek++;
+    if(num%tek==0 && tek%2!=0){
+        console.log(tek);
+    }
 }
