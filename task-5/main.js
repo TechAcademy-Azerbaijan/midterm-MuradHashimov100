ed1d99353ea99c4661fcad8b150f4588b1a86edf
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-06-21 19:34:45 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,12 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let a =-15;
+let b = 10;
+let sum = 0;
+while(a<b){
+    // debugger;
+    if(a%2!=0){
+        sum = sum+a;
+    }
+    a++;
+    
+}
+console.log(sum);
