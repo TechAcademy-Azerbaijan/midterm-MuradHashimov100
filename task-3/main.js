6ca8e903133bd8ca899f3d2f96518d0715fbfb41
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-06-21 20:45:56 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,15 +1,8 @@
-let num = 12;
-
-let tek;
-
-
-while(num>0){
-    // debugger;
-    // let tekinteki = num;
-   if(num%2!=0){
-    tek = num;
-    console.log(num);
+let num = 123456;
+// let temp;
+// while(num>0){
+//     let n = num%10;
+//     num = parseInt(num/10);
+//     console.log(n);
     
-   }
-   num--;
-}
+// }
